387f8d6754c32b0650c1490607413fbcb078a02b
Malo Grall <malo.grall@gmail.com>
2022-02-06 22:11:04 +0100

    Installed Babel for Jest Typescript

--- test/services/UNSport.test.ts
+++ test/services/UNSport.test.ts
@@ -1,4 +1,27 @@
+import { UnSport } from '../../src/services/UNSport'
+
 describe('UNSport class for manipulating API', () => {
+  const unsport = new UnSport()
+
+  beforeAll(async () => {
+    // await unsport.init()
+  })
+
+  test('well authenticated to Nantes University CAS', async () => {
+    expect(await unsport.bookSlot(0)).toEqual(
+      expect.objectContaining<Awaited<ReturnType<UnSport['bookSlot']>>>({
+        booked: true
+      })
+    )
+  })
+
   test.todo('fetch Sports, Slots, and Reservations')
-  test.todo('book a slot')
+
+  // test('book a slot', async () => {
+  //   expect(await unsport.bookSlot(590)).toEqual(
+  //     expect.objectContaining<Awaited<ReturnType<UnSport['bookSlot']>>>({
+  //       booked: true
+  //     })
+  //   )
+  // })
 })
